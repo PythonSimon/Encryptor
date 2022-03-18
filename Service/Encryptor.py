83536f7d5dd0f6fd5e8617efe537397eccf02daa
PythonSimon <8178778@qq.com>
2022-03-19 07:26:42 +0800

    Finish encrypt function and test(passed)

--- Service/Encryptor.py
+++ Service/Encryptor.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 
-codebook = {
+from random import *
+
+codebooks = {
     "p": "3141592653",
     "e": "2718281828",
-    "s": "1414213562"
+    "s": "1414213562",
+    "r": "5467558585",
+    "t": "8345829387"
 }
 
-digit = {
+digits = {
     "A": 0,
     "B": 1,
     "C": 2,
@@ -20,3 +24,21 @@ digit = {
 }
 
 
+def encrypt(plaintext: str):
+
+    # 获取 Unicode 列表
+    plainUnicodeLst = [ord(character) for character in plaintext]
+
+    # 选取密码本、密钥
+    codebookKey = choice(list(codebooks.keys()))
+    digitKey = choice(list(digits.keys()))
+    secretKey = codebooks[codebookKey][digits[digitKey]]
+
+    # 加密
+    cipherUnicodeList = [chr(unicode + int(secretKey)) for unicode in plainUnicodeLst]
+    ciphertext = codebookKey + digitKey + "/".join(cipherUnicodeList)
+
+    return ciphertext
+
+
+print(encrypt("Hello!你好！"))
